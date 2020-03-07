6389dfdf599353b7762bb1412cce0d9ebb3b29bb
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-03-06 21:03:07 -0400

    Creando la estructura del programa, y creando la Entidad Persona

---
+++ RegistroSQLLITE/Entidades/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RegistroSQLLITE.Entidades
+{
+    public class Persona
+    {
+        [Key]
+        public int PersonId { get; set; }
+        public string Nombres { get; set; }
+
+        public Persona()
+        {
+            PersonId = 0;
+            Nombres = string.Empty;
+        }
+    }
+}
